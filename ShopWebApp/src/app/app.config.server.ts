039260fbafa83a425d8ecc7250282598ae76d94f
Bhawesh Sharma <bhawesh.10827643@ltimindtree.com>
2025-06-20 21:30:07 +0530

    fixed dep

--- ShopWebApp/src/app/app.config.server.ts
+++ ShopWebApp/src/app/app.config.server.ts
@@ -1,10 +1,10 @@
-import { provideServerRendering } from '@angular/ssr';
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+// import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering(),
+   
   ]
 };
 
